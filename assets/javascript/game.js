ef92d66f0fe01701de3c4401a30b9728baae2480
atiftariq786 <atiftariq786@hotmail.com>
2019-02-27 01:01:39 -0800

    Modified index.html game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,62 +5,103 @@ var crystals = [0,0,0,0];
 var win = 0;
 var loss = 0;
 var score = 0;
-var resetGame = 0;
 
-
-
-
-
-// Main Random variable generator function 
 
  $(document).ready (function  (){
-
-
-    function mainRandNum() {
+   
+    // Main Random variable generator function -----------------------------
+    function RandNum() {
 
         mainRandNum = Math.floor(Math.random()* 102)+19;
-
+        $("#RandomNumber").text(mainRandNum);
     }
- 
+    RandNum();
 
- // Crystal Random variable generator function 
+ // Crystal Random variable generator function -------------------
 
     function assignCrystalRand(){
     
         for(var i = 0; i<crystals.length; i++){
 
           crystals[i] = Math.floor(Math.random()* 12)+1;
-
+          
         }
    }
+   assignCrystalRand ();
 
+     // Reset/New Game condition ----------------------------------------
 
- // win/loss condition 
+     function resetGame (){
+        RandNum();
+        score = 0;
+        assignCrystalRand();
+        
+        }
+  
+
+ // win/loss condition -------------------------------------
     
     function gameOperation (){
 
         if(score === mainRandNum) {
-
+        
         win += 1;
-        console.log("you won");
+        $("#wins").text(win);
+        resetGame ();
+        
         }
         else if(score > mainRandNum) {
-
+        
         loss += 1;
-        console.log("you loss");
+        
+        $("#loss").text(loss);
+        resetGame ();
+        
         }
-
+        
    
     }
+  
 
-// Reser/New Game condition 
-
-    function resetGame (){
-        mainRandNum();
-        score = 0;
-        assignCrystalRand();
+// game on click button operation -------------------------------------------
+    $(document).on("click", "#crystal1", 
+    function(){
         
-    }
+        score += crystals[0];
+        gameOperation();
+        $("#score").text(score);
+    })
+
+    $(document).on("click", "#crystal2", 
+    function(){
+        score += crystals[1];
+        gameOperation();
+        $("#score").text(score);
+    })
+
+    $(document).on("click", "#crystal3", 
+    function(){
+        
+        score += crystals[2];
+        gameOperation();
+        $("#score").text(score);
+    })
+
+    $(document).on("click", "#crystal4", 
+    function(){
+        
+        score += crystals[3];
+        gameOperation();
+        $("#score").text(score);
+    })
+
 
 
 })
+
+
+
+
+
+
+
